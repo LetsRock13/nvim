8a316ee5de66430f4b155a4b09d5674aa67745e6
LetsRock13 <r.hingst@web.de>
2024-08-14 09:05:55 +0200

    Added some options and some key remaps

--- lua/letsrock/options.lua
+++ lua/letsrock/options.lua
@@ -1,7 +1,25 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.numberwidth = 4
 
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
 vim.opt.ignorecase = true
+
+vim.opt.wrap = false
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
+vim.opt.showmode = false
+
+vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 8
+
+vim.opt.backup = false
+vim.opt.writebackup = false
+vim.opt.swapfile = false
+
+vim.opt.termguicolors = true
+
--- lua/letsrock/remap.lua
+++ lua/letsrock/remap.lua
@@ -1,2 +1,29 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Resize with arrows
+vim.keymap.set("n", "<C-Up>", ":resize -2<CR>")
+vim.keymap.set("n", "<C-Down>", ":resize +2<CR>")
+vim.keymap.set("n", "<C-Left>", ":vertical resize -2<CR>")
+vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>")
+
+-- Move text up and down
+vim.keymap.set("n", "<A-j>", "<Esc>:m .+1<CR>==gi")
+vim.keymap.set("n", "<A-k>", "<Esc>:m .-2<CR>==gi")
+
+-- Visual --
+-- Stay in indent mode
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
+
+-- Move text up and down
+vim.keymap.set("v", "<A-j>", ":m .+1<CR>==")
+vim.keymap.set("v", "<A-k>", ":m .-2<CR>==")
+vim.keymap.set("v", "p", '"_dP')
+
+-- Visual Block --
+-- Move text up and down
+vim.keymap.set("x", "J", ":move '>+1<CR>gv-gv")
+vim.keymap.set("x", "K", ":move '<-2<CR>gv-gv")
+vim.keymap.set("x", "<A-j>", ":move '>+1<CR>gv-gv")
+vim.keymap.set("x", "<A-k>", ":move '<-2<CR>gv-gv")
