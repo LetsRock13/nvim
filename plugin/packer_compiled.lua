bafc6bce16562dad39d47c0dd186c5e2d928e916
LetsRock13 <r.hingst@web.de>
2024-08-20 19:20:29 +0200

    Removed fugitiv lazygit is used instad

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -139,11 +139,6 @@ _G.packer_plugins = {
     loaded = true,
     path = "/home/robert/.local/share/nvim/site/pack/packer/start/undotree",
     url = "https://github.com/mbbill/undotree"
-  },
-  ["vim-fugitive"] = {
-    loaded = true,
-    path = "/home/robert/.local/share/nvim/site/pack/packer/start/vim-fugitive",
-    url = "https://github.com/tpope/vim-fugitive"
   }
 }
 
