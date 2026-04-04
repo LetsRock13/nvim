47430d878ed798cef4338cee6b76473fda44ef55
LetsRock13 <r.hingst@web.de>
2026-04-04 20:44:54 +0200

    added some comments

--- lua/letsrock/remap.lua
+++ lua/letsrock/remap.lua
@@ -15,23 +15,18 @@ vim.keymap.set("n", "<A-j>", ":m .+1<CR>")
 vim.keymap.set("n", "<A-k>", ":m .-2<CR>")
 
 -- Stay in indent mode
-vim.keymap.set("v", "<", "<gv")
-vim.keymap.set("v", ">", ">gv")
-vim.keymap.set("n", "J", "mzJ`z")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("n", "J", "mzJ`z") -- Entfernt das CR aus der unteren Zeile
+vim.keymap.set("n", "<C-d>", "<C-d>zz") -- Schnelles auf abbwert Scrollen
+vim.keymap.set("n", "<C-u>", "<C-u>zz") -- Schnelles auf abbwert Scrollen
+vim.keymap.set("n", "n", "nzzzv") -- Springe zwischen Gesuchtem auf und ab
+vim.keymap.set("n", "N", "Nzzzv") -- Springe zwischen Gesuchtem auf und ab
 
 -- keep the kopied word
-vim.keymap.set("x", "<leader>p", "\"_dP")
+vim.keymap.set("x", "<leader>p", "\"_dP") -- Paset
 
-vim.keymap.set("n", "<leader>y", "\"+y")
-vim.keymap.set("v", "<leader>y", "\"+y")
-vim.keymap.set("n", "<leader>Y", "\"+Y")
-
-vim.keymap.set("n", "<leader>d", "\"_d")
-vim.keymap.set("v", "<leader>d", "\"_d")
+vim.keymap.set("n", "<leader>y", "\"+y") -- Copy into Clipborad
+vim.keymap.set("v", "<leader>y", "\"+y") -- Copy im Blockmode into Clipborad
+vim.keymap.set("n", "<leader>Y", "\"+Y") -- Copy into Clipborad
 
 vim.keymap.set("i", "<C-c>", "<ESC>")
 -- Add a new line without insert mode
@@ -42,16 +37,14 @@ vim.keymap.set("n", "<A-p>", "Vyp<ESC>")
 vim.keymap.set("n", "Q", "<nop>")
 
 -- Move text up and down
-vim.keymap.set("v", "<A-j>", ":m .+1<CR>==")
 vim.keymap.set("v", "<A-k>", ":m .-2<CR>==")
+vim.keymap.set("v", "<A-j>", ":m .+1<CR>==")
 vim.keymap.set("v", "p", '"_dP')
 
 -- Visual Block --
 -- Move text up and down
 vim.keymap.set("x", "J", ":move '>+1<CR>gv-gv")
 vim.keymap.set("x", "K", ":move '<-2<CR>gv-gv")
-vim.keymap.set("x", "<A-j>", ":move '>+1<CR>gv-gv")
-vim.keymap.set("x", "<A-k>", ":move '<-2<CR>gv-gv")
 
 -- LazyGit --
 vim.keymap.set("n", "<leader>lg", ":LazyGit<CR>")
@@ -67,5 +60,3 @@ vim.keymap.set("n", "<leader>bn", ":bn<CR>") -- Next Buffer
 vim.keymap.set("n", "<leader>bp", ":bp<CR>") -- Previous Buffer
 vim.keymap.set("n", "<leader>bf", ":bf<CR>") -- First Buffer
 vim.keymap.set("n", "<leader>bd", ":bdelete<CR>") -- Delete current Buffe
-
-
