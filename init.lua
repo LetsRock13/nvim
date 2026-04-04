badbf52aa4478e6790d3fe6c03a0dc52889cbcd9
LetsRock13 <r.hingst@web.de>
2026-04-04 23:04:16 +0200

    redefining options.lua and remap.lua

--- init.lua
+++ init.lua
@@ -3,3 +3,5 @@
 -- vim.g.loaded_netwrPlugin = 1
 -- vim.g.loaded_node_provider = 0
 -- vim.g.loaded_perl_provider = 0
+require("options")
+require("remap")
---
+++ options.lua
@@ -0,0 +1,35 @@
+-- list of options
+local listofOptions = { 
+	number = true,
+	relativenumber = true,
+	numberwidth = 4,
+	hlsearch = false,
+	incsearch = true,
+	ignorecase = true,
+	smartcase = true,
+	wrap = true,
+	tabstop = 4,
+	shiftwidth = 4,
+	expandtab = true,
+	smartindent = true,
+	scrolloff = 8,
+	sidescrolloff = 8,
+	backup = false,
+	writebackup = false,
+	swapfile = false,
+	termguicolors = true,
+	syntax = on,
+}
+
+-- iterate throug any given option
+-- and it's value and set the value
+function setOption()
+
+  for key, value in pairs(listofOptions) do
+     vim.opt[key] = value
+  end
+
+end
+
+-- call function (obviously)
+setOption()
