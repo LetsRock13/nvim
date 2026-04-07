0b33decd0691c520258cd6a0ef9ae386bd9079e9
LetsRock13 <r.hingst@web.de>
2026-04-07 11:56:10 +0200

    removed not needed packages

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -40,13 +40,10 @@ return {
 	{'hrsh7th/cmp-path'},
 	{'hrsh7th/cmp-buffer'},
 	{'hrsh7th/cmp-calc'},
-	{'hrsh7th/cmp-calc'},
 	{'f3fora/cmp-spell'},
 	{'tamago324/cmp-zsh'},
 	{'quangnguyen30192/cmp-nvim-ultisnips'},
 	{'hrsh7th/cmp-nvim-lsp-signature-help'},
-	-- Vim-Be-Good
-	{'ThePrimeagen/vim-be-good'},
 	-- nvim v0.7.2
 	{
 	 'kdheepak/lazygit.nvim',
@@ -62,15 +59,10 @@ return {
 	{
 		"L3MON4D3/LuaSnip",
 		-- follow latest release.
-		version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+		--version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
 		dependencies = { "rafamadriz/friendly-snippets" },
 	},
 	{ 'saadparwaiz1/cmp_luasnip' },
-	{"atiladefreitas/dooing"},
-	{
-		'nvim-lualine/lualine.nvim',
-		dependencies = { 'nvim-tree/nvim-web-devicons' }
-	},
 }
 
 
