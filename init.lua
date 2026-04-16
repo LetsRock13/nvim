6fd32974ea57655cb57a4289765edb4907273a51
LetsRock13 <r.hingst@web.de>
2026-04-16 20:12:52 +0200

    disabled node

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 -- Disable netrw
 -- vim.g.loaded_netwr = 1
 -- vim.g.loaded_netwrPlugin = 1
--- vim.g.loaded_node_provider = 0
--- vim.g.loaded_perl_provider = 0
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
+
 require("common")
