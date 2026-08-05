e4c2a7ab6ee8aa34d7d4732d98734ef9ca8cff14
LetsRock13 <r.hingst@web.de>
2026-08-05 14:27:45 +0200

    removed terminal keymaping and added mapping for tabs

--- lua/letsrock/remap.lua
+++ lua/letsrock/remap.lua
@@ -59,13 +59,17 @@ vim.keymap.set("x", "K", ":move '<-2<CR>gv-gv")
 vim.keymap.set("n", "<leader>lg", ":LazyGit<CR>")
 
 -- Nvim Terminal remapping --
-vim.keymap.set("n", "<leader>t", "terminal<ESC>")
-vim.keymap.set("n", "<leader>ts", ":abov terminal<ESC>")
-vim.keymap.set("n", "<leader>tc", "")
-vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
+--vim.keymap.set("n", "<leader>t", "terminal<ESC>")
+--vim.keymap.set("n", "<leader>ts", ":abov terminal<ESC>")
+--vim.keymap.set("n", "<leader>tc", "")
+--vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
 
 -- Nvim Buffer Mapping
 vim.keymap.set("n", "<leader>bn", ":bn<CR>") -- Next Buffer
 vim.keymap.set("n", "<leader>bp", ":bp<CR>") -- Previous Buffer
 vim.keymap.set("n", "<leader>bf", ":bf<CR>") -- First Buffer
 vim.keymap.set("n", "<leader>bd", ":bdelete<CR>") -- Delete current Buffe
+
+-- Make current window/buffer "fullscreen"
+vim.keymap.set("n", "<leader>tt", ":tab split<CR>")
+vim.keymap.set("n", "<leader>tc", ":tabc<CR>")
